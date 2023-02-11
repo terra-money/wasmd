92a6f5783ac524bf0a4e811e7dfb5a5ddfe2821c
Jacob Gadikian <jacobgadikian@gmail.com>
2023-02-11 11:41:45 +0700

    weights in weights.go

--- app/params/params.go
+++ app/params/params.go
@@ -4,9 +4,4 @@ package params
 const (
 	StakePerAccount           = "stake_per_account"
 	InitiallyBondedValidators = "initially_bonded_validators"
-	DefaultWeightMsgMint
-	DefaultWeightMsgBurn
-	DefaultWeightMsgChangeAdmin
-	DefaultWeightMsgSetDenomMetadata
-	DefaultWeightMsgCreateDenom
 )
